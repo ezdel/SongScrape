9727107d6719e1f6952daa226350f8488c91706b
Eric Zuckerman <ericzuckerman@Erics-MacBook-Pro.local>
2016-11-05 11:03:46 -0700

    fixed listening port for deployment

--- server.js
+++ server.js
@@ -168,6 +168,7 @@ app.post('/songs/:id', function(req, res){
 
 
 // listen on port 3000
-app.listen(3000, function() {
-  console.log('App running on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+ console.log('App listening on PORT ' + PORT);
 });
